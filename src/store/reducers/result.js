c999835326a5a90c2449698fd709fe355817012e
Priya <guptapriyab@gmail.com>
2020-04-08 19:35:38 +0530

    making switch case more linear

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,17 +1,21 @@
 import * as actionTypes from '../action/actionTypes';
-import {updateObject} from '../utility';
+import { updateObject } from '../utility';
 
 const initialState = {
-    results:[]
+    results: []
 }
-const reducer = (state = initialState,action) =>{
-    console.log('state',state); 
-    switch (action.type){
+const deleteResult = (state, action) => {
+    const updatedResults = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, { results: updatedResults })
+}
+
+const reducer = (state = initialState, action) => {
+    console.log('state', state);
+    switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return updateObject(state,{results : state.results.concat([{value:action.result,id:new Date()}])})
+            return updateObject(state, { results: state.results.concat([{ value: action.result, id: new Date() }]) })
         case actionTypes.DELETE_RESULT:
-            const updatedResults = state.results.filter(result=>result.id!==action.resultElId);
-            return updateObject(state, {results:updatedResults})
+            return deleteResult(state, action);
     }
     return state;
 }
