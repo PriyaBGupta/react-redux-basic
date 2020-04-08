49b0b7a66add986abf1b0c1472a6d67c18a0ba1b
Priya <guptapriyab@gmail.com>
2020-04-08 18:46:28 +0530

    restructed  the file and added reduc thunk

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionCreator from '../../store/action/action';
+import * as actionCreator from '../../store/action/index';
 
 class Counter extends Component {
     state = {
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {createStore,combineReducers, applyMiddleware, compose} from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 const rootReducer = combineReducers({
     ctr:counterReducer,
     res:resultReducer
@@ -27,7 +28,7 @@ const logger = store => {
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //const store = createStore(reducer);
 //the below store is created using two splitted reduce file
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
