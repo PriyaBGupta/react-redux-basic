bdcc2a6673e99d74391df061aa9657c66962ac8f
Priya <guptapriyab@gmail.com>
2020-04-08 19:14:58 +0530

    Getting state in action creators

--- src/store/action/result.js
+++ src/store/action/result.js
@@ -7,9 +7,13 @@ export const saveResult = (result) => {
     }
 }
 export const storeResult = (result) => {
-    return dispatch => {
+    //there is also one more parameter beside dispatch which is getState to get the state of store but then dont use it too much
+    // instead u can pass the state to this function and then use it
+    return (dispatch, getState) => {
         // here you can use promise and get the data and once the data is thre then use dispatch to update the store
         setTimeout(()=>{
+            // const oldCounter = getState().ctr.counter;
+            // console.log(oldCounter);
             dispatch(saveResult(result));
         },2000);
     }
